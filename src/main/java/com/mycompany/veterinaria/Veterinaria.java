7d4f65fe594b4aa9eb900c0f83c91828e753cd74
eorbezom <eorbezo99@gmail.com>
2024-08-17 22:19:15 -0500

    primer commit del proyecto

---
+++ src/main/java/com/mycompany/veterinaria/Veterinaria.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.veterinaria;
+
+import com.mycompany.veterinaria.interfaz.Principal;
+
+/**
+ *
+ * @author usuario
+ */
+public class Veterinaria {
+
+    public static void main(String[] args) {
+        Principal interfazPrincipal = new Principal();
+        interfazPrincipal.setVisible(true);
+        interfazPrincipal.setLocationRelativeTo(interfazPrincipal);
+    }
+}
